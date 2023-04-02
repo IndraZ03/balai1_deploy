6f40834bff8cd7c8b06b6249ace6f700ad9a5337
IndraZ03 <96097446+IndraZ03@users.noreply.github.com>
2023-04-02 14:17:23 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://balaibmkg.github.io/balai1/';
+$config['base_url'] = 'http://balaibmkg.github.io/';
 
 /*
 |--------------------------------------------------------------------------
