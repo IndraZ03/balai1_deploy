159d60b5711dadd8105b8a1b84fffa4adb9f995a
IndraZ03 <96097446+IndraZ03@users.noreply.github.com>
2023-04-02 14:30:20 +0700

    Perubahan base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://balaibmkg.github.io/';
+$config['base_url'] = 'https://indraz03.github.io/balai1/';
 
 /*
 |--------------------------------------------------------------------------
